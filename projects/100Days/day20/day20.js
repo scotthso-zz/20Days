8dacaf07b3b92dd8c780388b1e527d31e01383e9
Scott So <scotthhso@gmail.com>
2014-05-15 16:28:11 -0400

    Day 20 update

--- projects/100Days/day20/day20.js
+++ projects/100Days/day20/day20.js
@@ -1,3 +1,11 @@
-var year;
-var month;
-var date;
+var year = new Date().getFullYear;
+var month = new Date().getMonth;
+var date = new Date().getDay;
+
+function calculate () {
+    
+var yeard = year -     
+ var resultSeconds  
+    
+}
+    
