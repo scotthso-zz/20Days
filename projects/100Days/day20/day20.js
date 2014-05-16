8465dc1bf98cf96e345aa8f8efbc955d66269f3d
Scott So <scottso@crescentschool.org>
2014-05-16 10:20:40 -0400

    minor updates

--- projects/100Days/day20/day20.js
+++ projects/100Days/day20/day20.js
@@ -1,11 +1,17 @@
-var year = new Date().getFullYear;
-var month = new Date().getMonth;
-var date = new Date().getDay;
+var year = new Date().getFullYear();
+var month = new Date().getMonth();
+var date = new Date().getDay();
 
 function calculate () {
     
-var yeard = year -     
- var resultSeconds  
+var yeard = year - document.getElementById("year").value; 
+var monthd = month - document.getElementById("month").value;
+var dated = day - document.getElementById("day").value;
     
+var resultSeconds = yeard*365*24*60*60 + monthd*30*60*60 + dated*24*60*60; 
+
+    document.getElementById("seconds-display").innerHTML = month;
+
+ 
 }
     
