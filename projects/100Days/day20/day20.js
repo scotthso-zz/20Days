2b1065e5d18e221eb39d204de08e49c981d1fa17
Scott So <scotthhso@gmail.com>
2014-05-16 11:16:12 -0400

    Day 20 and blog updates

--- projects/100Days/day20/day20.js
+++ projects/100Days/day20/day20.js
@@ -1,16 +1,16 @@
 var year = new Date().getFullYear();
 var month = new Date().getMonth();
-var date = new Date().getDay();
+
 
 function calculate () {
     
 var yeard = year - document.getElementById("year").value; 
 var monthd = month - document.getElementById("month").value;
-var dated = day - document.getElementById("day").value;
-    
-var resultSeconds = yeard*365*24*60*60 + monthd*30*60*60 + dated*24*60*60; 
 
-    document.getElementById("seconds-display").innerHTML = month;
+    
+var resultSeconds = yeard*365*24*60*60 + (monthd*30*24*60*60); 
+
+document.getElementById("seconds-display").innerHTML =  "You have lived for approximately " + resultSeconds + " seconds. Not taking into account different amount of days for each month and date.";
 
  
 }
