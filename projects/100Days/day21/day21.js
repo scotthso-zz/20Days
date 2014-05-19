0dc7fbd5baa7e1e168ea284c9a6d3f8948e12f81
Scott So <scotthhso@gmail.com>
2014-05-19 13:32:10 -0400

    scrapped the php project

--- projects/100Days/day21/day21.js
+++ projects/100Days/day21/day21.js
@@ -1,87 +1,16 @@
-/* 
-Created by: Kenrick Beckett
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-Name: Chat Engine
-*/
+server.listen(80);
 
-var instanse = false;
-var state;
-var mes;
-var file;
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
-function Chat () {
-    this.update = updateChat;
-    this.send = sendChat;
-	this.getState = getStateOfChat;
-}
-
-//gets the state of the chat
-function getStateOfChat(){
-	if(!instanse){
-		 instanse = true;
-		 $.ajax({
-			   type: "POST",
-			   url: "process.php",
-			   data: {  
-			   			'function': 'getState',
-						'file': file
-						},
-			   dataType: "json",
-			
-			   success: function(data){
-				   state = data.state;
-				   instanse = false;
-			   },
-			});
-	}	 
-}
-
-//Updates the chat
-function updateChat(){
-	 if(!instanse){
-		 instanse = true;
-	     $.ajax({
-			   type: "POST",
-			   url: "process.php",
-			   data: {  
-			   			'function': 'update',
-						'state': state,
-						'file': file
-						},
-			   dataType: "json",
-			   success: function(data){
-				   if(data.text){
-						for (var i = 0; i < data.text.length; i++) {
-                            $('#chat-area').append($("<p>"+ data.text[i] +"</p>"));
-                        }								  
-				   }
-				   document.getElementById('chat-area').scrollTop = document.getElementById('chat-area').scrollHeight;
-				   instanse = false;
-				   state = data.state;
-			   },
-			});
-	 }
-	 else {
-		 setTimeout(updateChat, 1500);
-	 }
-}
-
-//send the message
-function sendChat(message, nickname)
-{       
-    updateChat();
-     $.ajax({
-		   type: "POST",
-		   url: "process.php",
-		   data: {  
-		   			'function': 'send',
-					'message': message,
-					'nickname': nickname,
-					'file': file
-				 },
-		   dataType: "json",
-		   success: function(data){
-			   updateChat();
-		   },
-		});
-}
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
